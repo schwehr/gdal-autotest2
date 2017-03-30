b36afa9198c576bb0f92a86c59d8c38bf55b7c0d
Kurt Schwehr <schwehr@google.com>
2017-03-30 12:21:24 -0700

    Add rdataset test and fuzzer.

---
+++ cpp/frmts/r/rdataset_fuzzer.cc
@@ -0,0 +1,49 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stddef.h>
+#include <stdint.h>
+#include <memory>
+#include <string>
+
+#include "base/logging.h"
+#include "third_party/absl/memory/memory.h"
+#include "autotest2/cpp/util/vsimem.h"
+#include "frmts/r/rdataset.h"
+#include "gcore/gdal.h"
+#include "gcore/gdal_priv.h"
+
+class WithQuietHandler {
+ public:
+  WithQuietHandler() { CPLPushErrorHandler(CPLQuietErrorHandler); }
+  ~WithQuietHandler() { CPLPopErrorHandler(); }
+};
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  const char kFilename[] = "/vsimem/a.rda";
+  const string data2(reinterpret_cast<const char *>(data), size);
+  autotest2::VsiMemTempWrapper wrapper(kFilename, data2);
+
+  {
+    WithQuietHandler error_handler;
+    auto open_info =
+        gtl::MakeUnique<GDALOpenInfo>(kFilename, GDAL_OF_READONLY, nullptr);
+    int result = RDataset::Identify(open_info.get());
+    CHECK_LE(-1, result);
+    CHECK_GE(1, result);
+    auto dataset = gtl::WrapUnique(RDataset::Open(open_info.get()));
+  }
+
+  return 0;
+}
---
+++ cpp/frmts/r/rdataset_test.cc
@@ -0,0 +1,41 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Tests R raster driver.
+//
+// See also:
+//   http://www.gdal.org/frmt_r.html
+//   https://trac.osgeo.org/gdal/browser/trunk/autotest/gdrivers/r.py
+
+#include "frmts/r/rdataset.h"
+
+#include "file/base/path.h"
+#include "gunit.h"
+#include "third_party/absl/memory/memory.h"
+#include "gcore/gdal.h"
+#include "gcore/gdal_priv.h"
+
+namespace autotest2 {
+namespace {
+
+TEST(RDatasetTest, IdentifyDoesNotExist) {
+  auto open_info = gtl::MakeUnique<GDALOpenInfo>("/does_not_exist",
+                                                 GDAL_OF_READONLY, nullptr);
+  EXPECT_EQ(FALSE, RDataset::Identify(open_info.get()));
+}
+
+// TODO(schwehr): More tests.
+
+}  // namespace
+}  // namespace autotest2
